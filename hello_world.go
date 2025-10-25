670c16c9163e609da725d168777b6b2ef606670e
Georgios Hadjiharalambous <georgiosh@speechmatics.com>
2025-10-25 21:23:45 +0100

    use switch statement, and improve functions by separeting functionality, write usefull comments for the languages structure

--- hello_world.go
+++ hello_world.go
@@ -2,24 +2,49 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+/**
+In our function signature we have made a named return value (prefix string).
+
+This will create a variable called prefix in your function.
+
+It will be assigned the "zero" value. This depends on the type, for example ints are 0 and for strings it is "".
+
+You can return whatever it's set to by just calling return rather than return prefix.
+**/
+
+/**
+The function name starts with a lowercase letter. In Go, public functions start with a capital letter,
+and private ones start with a lowercase letter. We don't want the internals of our algorithm exposed to the world,
+so we made this function private
+**/
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	return
 }
 
 func main() {
