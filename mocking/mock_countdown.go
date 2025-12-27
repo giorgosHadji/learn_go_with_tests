575d0108889d41954e31a34f8e2bfcf34e5814c2
Georgios Hadjiharalambous <40407855+giorgosHadji@users.noreply.github.com>
2025-12-27 16:45:46 +0000

    add configureable sleeper struct to test duration of sleep

--- mocking/mock_countdown.go
+++ mocking/mock_countdown.go
@@ -52,7 +52,24 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 const write = "write"
 const sleep = "sleep"
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func main() {
-	// main_sleeper := DefaultSleeper{}
-	Countdown(os.Stdout, &DefaultSleeper{})
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 }
