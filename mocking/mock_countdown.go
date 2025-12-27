2b15178126b03a3ff72c4975d36db010620fdd6e
Georgios Hadjiharalambous <40407855+giorgosHadji@users.noreply.github.com>
2025-12-27 16:37:59 +0000

    mock both inputs of countDown function to verify order of calls made

--- mocking/mock_countdown.go
+++ mocking/mock_countdown.go
@@ -36,6 +36,22 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 	fmt.Fprint(out, finalWord)
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
 func main() {
 	// main_sleeper := DefaultSleeper{}
 	Countdown(os.Stdout, &DefaultSleeper{})
