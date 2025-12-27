4474252d1661a5631e0d36548dc83279c8be9b54
Georgios Hadjiharalambous <40407855+giorgosHadji@users.noreply.github.com>
2025-12-27 16:27:52 +0000

    initial mocking

--- mocking/mock_test.go
+++ mocking/mock_test.go
@@ -7,8 +7,9 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -20,4 +21,7 @@ Go!`
 		t.Errorf("got %q want %q", got, want)
 	}
 
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+	}
 }
