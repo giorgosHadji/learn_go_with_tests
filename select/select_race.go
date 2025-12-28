c5be8687c6e6e2fa44040b3b02f5659e5bf7fc11
Georgios Hadjiharalambous <40407855+giorgosHadji@users.noreply.github.com>
2025-12-28 17:13:15 +0000

    refactor to use select

--- select/select_race.go
+++ select/select_race.go
@@ -2,22 +2,31 @@ package select_test
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
+	/*you can wait for values to be sent to a channel with myVar := <-ch. This is a blocking call, as you're waiting for a value.
 
-	if aDuration < bDuration {
+	select allows you to wait on multiple channels. The first one to send a value "wins" and the code underneath the case is executed.*/
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	/*Always make channels
+
+	  Notice how we have to use make when creating a channel; rather than say var ch chan struct{}. When you use var the variable will be initialised with the "zero" value of the type. So for string it is "", int it is 0, etc.
+
+	  For channels the zero value is nil and if you try and send to it with <- it will block forever because you cannot send to nil channels
+	*/
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
