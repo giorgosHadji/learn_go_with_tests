619178221b80b8cf5b027a7fc35816ab9a00be11
Georgios Hadjiharalambous <40407855+giorgosHadji@users.noreply.github.com>
2025-12-28 16:53:05 +0000

    proper use of channels, and fixing of race condition of writing to map from many goroutines

--- concurrency/conc.go
+++ concurrency/conc.go
@@ -1,18 +1,28 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
+
+// find race conditions with  go test -race .
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func() {
-			results[url] = wc(url)
+			resultChannel <- result{url, wc(url)}
 		}()
 	}
-	// find race conditions with  go test -race .
-	time.Sleep(2 * time.Second)
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
 	return results
 }
